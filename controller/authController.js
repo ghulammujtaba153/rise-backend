8d4a707e43e0a8241419387280a4c1f1cd0ec569
ghulammujtaba153 <ghulammujtaba.dro@gmail.com>
2025-11-20 00:38:16 +0500

    lower case email

--- controller/authController.js
+++ controller/authController.js
@@ -1,145 +1,141 @@
-import User from "../models/userSchema.js"
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import User from "../models/userSchema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const createUser = async (req, res) => {
-    const {name, email, password, role} = req.body
+  const { name, email, password, role } = req.body;
 
-    try{
-        const data={
-            name,
-            email: email.toLowerCase().trim(),
-        }
-        
-        const encryptPassword = await bcrypt.hash(password, 10)
+  try {
+    const data = {
+      name,
+      email: email.toLowerCase().trim(),
+    };
 
-        data.password = encryptPassword
-        
-        if(role){
-            data.role = role
-        }
+    const encryptPassword = await bcrypt.hash(password, 10);
 
-         const user = await User.create(data)
+    data.password = encryptPassword;
 
-        
-        res.status(201).json({user})
-    }catch(error){
-        console.log(error)
+    if (role) {
+      data.role = role;
     }
-}
 
+    const user = await User.create(data);
+
+    res.status(201).json({ user });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const loginUser = async (req, res) => {
-    const {email, password, FCMToken} = req.body
+  const { email, password, FCMToken } = req.body;
 
-    try{
-        const lowcaseEmail = email.toLowerCase().trim()
-        const user = await User.findOne({email: lowcaseEmail})
-        if(!user){
-            return res.status(404).json({message: "User not found"})
-        }
-        const isPasswordCorrect = await bcrypt.compare(password, user.password)
-        if(!isPasswordCorrect){
-            return res.status(401).json({message: "Invalid password"})
-        }
+  try {
+    const trimmedEmail = email.trim();
 
-        if(FCMToken){
-            user.FCMToken = FCMToken
-            await user.save()
-        }
-        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1h"})
-        res.status(200).json({token, user})
-    }catch(error){
-        console.log(error)
-        res.status(500).json({message: "Internal server error"})
+    const user = await User.findOne({
+      email: { $regex: new RegExp(`^${trimmedEmail}$`, "i") },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
 
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    if (FCMToken) {
+      user.FCMToken = FCMToken;
+      await user.save();
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const getUser = async (req, res) => {
-    const {id} = req.params
-    try{
-        const user = await User.findById(id)
-        if(!user){
-            return res.status(404).json({message: "User not found"})
-        }
-        res.status(200).json({user})
-    }catch(error){
-        res.status(500).json({message: "Internal server error"})
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
-
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const updateUser = async (req, res) => {
-    const {id} = req.params
-    const {avatar,name,email, password, role} = req.body
+  const { id } = req.params;
+  const { avatar, name, email, password, role } = req.body;
 
-    try {
-        const data={
-            name,
-            email,
-        }
+  try {
+    const data = {
+      name,
+      email,
+    };
 
-        if(avatar){
-            data.avatar = avatar
-        }
-
-        if(password){
-            const encryptPassword = await bcrypt.hash(password, 10)
-            data.password = encryptPassword
-        }
-
-        if(role){
-            data.role = role
-        }
-
-        const user = await User.findByIdAndUpdate(id, data, {new: true})
-        res.status(200).json({user})
-
-    } catch (error) {
-        res.status(500).json({message: "Internal server error"})
+    if (avatar) {
+      data.avatar = avatar;
     }
-}
 
+    if (password) {
+      const encryptPassword = await bcrypt.hash(password, 10);
+      data.password = encryptPassword;
+    }
+
+    if (role) {
+      data.role = role;
+    }
+
+    const user = await User.findByIdAndUpdate(id, data, { new: true });
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const getAllUsers = async (req, res) => {
-    
-    try {
-        const users = await User.find()
-        res.status(200).json({users})
-    } catch (error) {
-        res.status(500).json({message: "Internal server error"})
-    }
-}
-
-
+  try {
+    const users = await User.find();
+    res.status(200).json({ users });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const forgotPassword = async (req, res) => {
-    const {email, newPassword} = req.body
-    try {
-        const user = await User.findOne({email})
-        if(!user){
-            return res.status(404).json({message: "User not found"})
-        }
-        const encryptPassword = await bcrypt.hash(newPassword, 10)
-        user.password = encryptPassword
-        await user.save()
-        res.status(200).json({message: "Password updated successfully"})
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({message: "Internal server error"})
+  const { email, newPassword } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
-
-
+    const encryptPassword = await bcrypt.hash(newPassword, 10);
+    user.password = encryptPassword;
+    await user.save();
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const deleteUser = async (req, res) => {
-    const {id} = req.params
-    
-    try {
-        const user = await User.findByIdAndDelete(id)
-        res.status(200).json({message: "User deleted successfully"})
-    } catch (error) {
-        res.status(500).json({message: "Internal server error"})
-    }
-}
+  const { id } = req.params;
+
+  try {
+    const user = await User.findByIdAndDelete(id);
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
